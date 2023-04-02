9bf07685fba6b2d0374333644d474f6ac3b147a3
MRJackFrost <69490107+MRJackFrost@users.noreply.github.com>
2023-04-02 18:39:59 -0300

    update

--- While.cpp
+++ While.cpp
@@ -1,16 +1,16 @@
 // Online C++ compiler to run C++ program online
-#include <iostream>
-using namespace std;
+#include <iostream> //Puxando uma biblioteca externa para receber valor do usuario
+using namespace std;//incluindo biblioteca padrão pra remover o std::
 
-int main() {
-    int num, i = 1;
+int main() { //abrindo a estrutura do codigo principal
+    int num, i = 1; //definindo as variavels e o valor de i
     
-    cout <<"digite um numero";
-    cin >> num;
+    cout <<"digite um numero"; //enviando uma informação para a interface do usuario
+    cin >> num; //recevendo um valor do usuario
     
-    while(i <=num){
-        cout <<i <<", ";
-        i++;
+    while(i <=num){ //enquanto i for menor ou igual a num faça
+        cout <<i <<", "; //mostre o valor para o usuario
+        i++; //adicione mais 1 no valor de i
     }
 
     return 0;
