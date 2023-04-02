dd3f64f7b7468f4e5376fcf1d869144d4f7e1cde
MRJackFrost <69490107+MRJackFrost@users.noreply.github.com>
2023-04-02 19:18:25 -0300

    Update

--- ChamandoFunção.cpp
+++ ChamandoFunção.cpp
@@ -1,13 +1,12 @@
-// Online C++ compiler to run C++ program online
-#include <iostream>
-using namespace std;
+#include <iostream> //Puxando uma biblioteca externa para receber valor do usuario
+using namespace std; //Incluindo biblioteca padrão
 
-int soma(int a, int b){
-    int Resultado;
-    Resultado = a+b;
-    return Resultado;
+int soma(int a, int b){ //abrindo uma função
+    int Resultado; //definindo uma variavel
+    Resultado = a+b; // definindo o valor da variavel
+    return Resultado; //retornando o valor para a funação
 }
 
-int main(){
-    cout << soma(6,4)<<endl;
+int main(){ //abrindo o codigo principal
+    cout << soma(6,4)<<endl; //chamando a função com os valores e mostrando para o usuario
 }
