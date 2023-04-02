1c7ec379638f84261192e9d442a3c0cf781ca931
MRJackFrost <69490107+MRJackFrost@users.noreply.github.com>
2023-04-02 19:04:24 -0300

    update

--- For.cpp
+++ For.cpp
@@ -1,17 +1,16 @@
-// Online C++ compiler to run C++ program online
-#include <iostream>
-using namespace std;
+#include <iostream> //Incluindo uma biblioteca externa para receber valor do usuario
+using namespace std; //Puxando a biblioteca externa padrão 
 
-int main() {
-    int num;
+int main() { //abrindo o bloco da estrutura principal de codigo
+    int num; //definindo a variavel
    
-    cout <<"digite um numero positivo" << endl;
-    cin >> num;
+    cout <<"digite um numero positivo" << endl; //mensagem para a interface do usuario
+    cin >> num; //solicitando o valor para o usuario
     
-    cout <<"Os primeiros " << num << " Numeros pares são: ";
+    cout <<"Os primeiros " << num << " Numeros pares são: "; //mensagem para o usuario
     
-    for(int i = 2; i <= num * 2; i += 2){
-        cout << i<< ", ";
+    for(int i = 2; i <= num * 2; i += 2){ //definindo o valor de i; enquanto i for menor igual ao valor de num multiplicado por 2 some
+        cout << i<< ", "; //mostre o valor de i pro usuario
     }
 
     return 0;
