c4500351505d474adec63047e08502aeaa458552
MRJackFrost <69490107+MRJackFrost@users.noreply.github.com>
2023-04-02 19:13:05 -0300

    Update

--- Character.cpp
+++ Character.cpp
@@ -1,23 +1,22 @@
-// Online C++ compiler to run C++ program online
-#include <iostream>
-#include <string>
-using namespace std;
+#include <iostream> //Puxando uma biblioteca externa para receber valor do usuario
+#include <string> //biblioteca externa de string
+using namespace std; //biblioteca padrão para remover o std::
 
-int main() {
-    int life;
-    float energy;
-    bool status;
-    string character;
+int main() { //abrindo o bloco de codigo principal
+    int life;  //definindo a variavel
+    float energy; //definindo a variavel
+    bool status; //definindo a variavel
+    string character;//definindo a variavel
     
-    life = 5;
-    energy = 99.99f;
-    status = true;
-    character = "Zero";
+    life = 5; //definindo valor para a varivel
+    energy = 99.99f;//definindo valor para a varivel
+    status = true;//definindo valor para a varivel
+    character = "Zero";//definindo valor para a varivel
     
-    cout << "Life: "<< life << endl;
-    cout << "Energy: "<< energy << endl;
-    cout << "Status: "<< status << endl;
-    cout << "Character name: "<< character << endl;
+    cout << "Life: "<< life << endl; //mostrando os valores na interface do usuario
+    cout << "Energy: "<< energy << endl;//mostrando os valores na interface do usuario
+    cout << "Status: "<< status << endl;//mostrando os valores na interface do usuario
+    cout << "Character name: "<< character << endl;//mostrando os valores na interface do usuario
     
 
     return 0;
