87da2449f26538c2fdaf1f6a4d255f1cdaf01379
MRJackFrost <69490107+MRJackFrost@users.noreply.github.com>
2023-04-02 19:16:19 -0300

    Update

--- ChamandoFunçõesMatematicas.cpp
+++ ChamandoFunçõesMatematicas.cpp
@@ -1,31 +1,30 @@
-// Online C++ compiler to run C++ program online
-#include <iostream>
-using namespace std;
+#include <iostream> //Puxando uma biblioteca externa para receber valor do usuario
+using namespace std; //incluindo biblioteca padrão
 
-int soma(int a, int b){
-    int Resultado;
-    Resultado = a+b;
-    return Resultado;
+int soma(int a, int b){ //abrindo uma função
+    int Resultado; //definindo variavel
+    Resultado = a+b; // definindo valor
+    return Resultado; //retornando o valor
 }
-int sub(int a, int b){
-    int Resultado;
-    Resultado = a-b;
-    return Resultado;
+int sub(int a, int b){ //abrindo uma função
+    int Resultado; //definindo variavel
+    Resultado = a-b; // definindo valor
+    return Resultado; //retornando o valor
 }
-int mult(int a, int b){
-    int Resultado;
-    Resultado = a*b;
-    return Resultado;
+int mult(int a, int b){ //abrindo uma função
+    int Resultado; //definindo variavel
+    Resultado = a*b; // definindo valor
+    return Resultado; //retornando o valor
 }
-int divs(int a, int b){
-    int Resultado;
-    Resultado = a/b;
-    return Resultado;
+int divs(int a, int b){ //abrindo uma função
+    int Resultado; //definindo variavel
+    Resultado = a/b; // definindo valor
+    return Resultado; //retornando o valor
 }
 
 int main(){
-    cout << soma(6,4)<<endl;
-    cout << sub(6,4)<<endl;
-    cout << mult(6,4)<<endl;
-    cout << divs(6,4)<<endl;
+    cout << soma(6,4)<<endl;//chamando a função e passando os valores para ela 
+    cout << sub(6,4)<<endl;//chamando a função e passando os valores para ela
+    cout << mult(6,4)<<endl;//chamando a função e passando os valores para ela
+    cout << divs(6,4)<<endl;//chamando a função e passando os valores para ela
 }
