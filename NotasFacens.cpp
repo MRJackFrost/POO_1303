5a32806d484a31048e96b4c25352cc9ac37ff114
MRJackFrost <69490107+MRJackFrost@users.noreply.github.com>
2023-04-02 19:00:17 -0300

    Update

--- NotasFacens.cpp
+++ NotasFacens.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-#include <stdlib.h>
-using namespace std;
+#include <iostream>//Puxando uma biblioteca externa para receber valor do usuario
+#include <stdlib.h>//incluindo biblioteca externa
+using namespace std;//incluindo biblioteca padrão
 
-int main() {
-    double ac1, ac2, af;
-    cout << "Digite sua AC1:" << endl;
-    cin >> ac1;
-    cout << "Digite sua AC2:" << endl;
-    cin >> ac2;
-    cout << "Digite sua Avaliação Final:" << endl;
-    cin >> af;
+int main() { //abrindo  a estrutura do codigo principal
+    double ac1, ac2, af; //definindo as variaveis
+    cout << "Digite sua AC1:" << endl; //mensagem na interface do usuario
+    cin >> ac1; //solicitando o valor da ac1
+    cout << "Digite sua AC2:" << endl;//mensagem na interface do usuario
+    cin >> ac2;//solicitando o valor da ac2
+    cout << "Digite sua Avaliação Final:" << endl;//mensagem na interface do usuario
+    cin >> af;//solicitando o valor da af
     
-    ac1 = (ac1 * 0.15);
-    ac2 = (ac2 * 0.35);
-    af = (af * 0.5);
+    ac1 = (ac1 * 0.15); //definindo o peso de 1,5
+    ac2 = (ac2 * 0.35); //definindo o peso de 3,5
+    af = (af * 0.5); //definindo o peso de 5
     
-    double soma = (ac1 + ac2 + af);
+    double soma = (ac1 + ac2 + af); //somando as notas
     
-    if(soma > 5){
-        cout << "Parabens você passou com: "<< soma;
+    if(soma > 5){ //se a nota for maior que 5
+        cout << "Parabens você passou com: "<< soma; //mensagem para o usuario
     }
-    if(soma == 5){
-        cout << "Quase morreu na praia com: " << soma;
+    if(soma == 5){ //se a nota for exatamente 5
+        cout << "Quase morreu na praia com: " << soma;//mensagem para o usuario
     }
-    if(soma < 5) {
-     cout << "Você foi reprovado com: " << soma;   
+    if(soma < 5) { //se a nota for menor que 5
+     cout << "Você foi reprovado com: " << soma;   //mensagem para o usuario
     }
     return 0;
 }
