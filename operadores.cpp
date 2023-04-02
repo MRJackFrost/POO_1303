05aefda5e292b9c6b7c4510d489f08dfe7c83222
MRJackFrost <69490107+MRJackFrost@users.noreply.github.com>
2023-04-02 18:33:06 -0300

    Update

--- operadores.cpp
+++ operadores.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //Puxando uma biblioteca externa para receber valor do usuario
+using namespace std;//Incluindo biblioteca padrão
 
-int main() {
-   int num =10, num2 = 4;
-   int soma, multi, divi, modulo, subt;
+int main() { //abrindo a estrutura do codigo
+   int num =10, num2 = 4; //definindo os valores de num e num2
+   int soma, multi, divi, modulo, subt; //definindo as variaveis
    
-   soma = num + num2;
-   multi = num * num2;
-   divi = num / num2;
-   modulo = num % num2;
-   subt = num - num2;
+   soma = num + num2; //somando as variaveis
+   multi = num * num2; //multiplicando as variaveis
+   divi = num / num2; // dividindo as variaveis
+   modulo = num % num2; //calculando o resto das variaveis
+   subt = num - num2; //subtraindo as variaveis
    
-   cout << "soma: "<<soma<<endl;
+   cout << "soma: "<<soma<<endl; //mostrando todos os resultados na interface para o usuario
    cout << "Multiplicação: "<<multi<<endl;
    cout << "Divisão: "<<divi<<endl;
    cout << "Modulo: "<<modulo<<endl;
